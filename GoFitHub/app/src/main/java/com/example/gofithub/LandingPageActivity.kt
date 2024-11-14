cbf40045e5bbd2dc03ef97b9ab728cd24affde3f
Binod <bnodtwari111@gmail.com>
2024-11-14 13:06:39 -0600

    Added Login views and its layout. On press user login it redirects to login page

--- GoFitHub/app/src/main/java/com/example/gofithub/LandingPageActivity.kt
+++ GoFitHub/app/src/main/java/com/example/gofithub/LandingPageActivity.kt
@@ -1,6 +1,8 @@
 package com.example.gofithub
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -12,5 +14,21 @@ class LandingPageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_landing_page)
+        // Set up button click listeners to open LoginActivity
+        findViewById<Button>(R.id.buttonUserLogin).setOnClickListener {
+            openLoginActivity()
+        }
+
+        findViewById<Button>(R.id.buttonTrainerLogin).setOnClickListener {
+            openLoginActivity()
+        }
     }
-}
+
+    private fun openLoginActivity() {
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
+    }
+    }
+
+
+

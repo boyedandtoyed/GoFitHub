b53889fda14f7bad25cd0a62666b91491817d172
Binod <bnodtwari111@gmail.com>
2024-11-14 15:55:44 -0600

    Added trainer registeration page and layout along with redirection

--- GoFitHub/app/src/main/java/com/example/gofithub/RegisterActivity.kt
+++ GoFitHub/app/src/main/java/com/example/gofithub/RegisterActivity.kt
@@ -16,12 +16,12 @@ class RegisterActivity : AppCompatActivity() {
         val trainerButton: Button = findViewById(R.id.buttonTrainerRegister)
         val generalUserButton: Button = findViewById(R.id.buttonGeneralUserRegister)
 
-//        trainerButton.setOnClickListener {
-//            Toast.makeText(this, "Trainer selected", Toast.LENGTH_SHORT).show()
-//            val intent = Intent(this, TrainerRegisterActivity::class.java)
-//                startActivity(intent)
-//
-//        }
+        trainerButton.setOnClickListener {
+            Toast.makeText(this, "Trainer selected", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, TrainerRegisterActivity::class.java)
+                startActivity(intent)
+
+        }
 
         generalUserButton.setOnClickListener {
             Toast.makeText(this, "General User selected", Toast.LENGTH_SHORT).show()
---
+++ GoFitHub/app/src/main/java/com/example/gofithub/TrainerRegisterActivity.kt
@@ -0,0 +1,38 @@
+package com.example.gofithub
+
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class TrainerRegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_trainer_register)
+
+        // Set up the experience level Spinner
+        val experienceLevelSpinner = findViewById<Spinner>(R.id.experienceLevelSpinner)
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.experience_levels,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            experienceLevelSpinner.adapter = adapter
+        }
+
+        // Set up the specialty Spinner
+        val specialtySpinner = findViewById<Spinner>(R.id.specialtySpinner)
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.specialties,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            specialtySpinner.adapter = adapter
+        }
+    }
+}

7ff7556b05407cf81a644c9275657ed599f3a007
Binod <bnodtwari111@gmail.com>
2024-11-27 04:11:31 -0600

    Added Dataentry page xml and then added its code (Have not implemented the database saving part yet)

--- GoFitHub/app/src/main/java/com/example/gofithub/ActivityEnteringPage.kt
+++ GoFitHub/app/src/main/java/com/example/gofithub/ActivityEnteringPage.kt
@@ -1,6 +1,9 @@
 package com.example.gofithub
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -12,5 +15,33 @@ class ActivityEnteringPage : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_entering_page)
 
+        val etCaloriesBurned = findViewById<EditText>(R.id.etCaloriesBurned)
+        val etAsanas = findViewById<EditText>(R.id.etPace) // Asanas field
+        val etDistance = findViewById<EditText>(R.id.etDistance)
+        val etTime = findViewById<EditText>(R.id.etTime)
+        val etSpeed = findViewById<EditText>(R.id.etSpeed)
+        val btnSubmitRunning = findViewById<Button>(R.id.btnSubmitRunning)
+
+        // Handling Save button
+        btnSubmitRunning.setOnClickListener {
+            val calories = etCaloriesBurned.text.toString().trim()
+            val asanas = etAsanas.text.toString().trim()
+            val distance = etDistance.text.toString().trim()
+            val time = etTime.text.toString().trim()
+            val speed = etSpeed.text.toString().trim()
+
+            // Checking input types
+            if (calories.isEmpty() || asanas.isEmpty() || distance.isEmpty() || time.isEmpty() || speed.isEmpty()) {
+                Toast.makeText(this, "Please fill out all fields!", Toast.LENGTH_SHORT).show()
+            } else {
+                saveActivityData(calories, asanas, distance, time, speed)
+                Toast.makeText(this, "Activity Saved!", Toast.LENGTH_SHORT).show()
+                finish()
+            }
+        }
+    }
+
+    private fun saveActivityData(calories: String, asanas: String, distance: String, time: String, speed: String) {
+    //do something here
     }
 }
